5118a2fe97827779ba8c624cbea4b0b40a942d92
keisato848 <72418986+keisato848@users.noreply.github.com>
2020-11-21 17:19:54 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,9 +35,13 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
 =======
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+>>>>>>> Stashed changes
+=======
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
 >>>>>>> Stashed changes
       @week_days.push(days)
     end
